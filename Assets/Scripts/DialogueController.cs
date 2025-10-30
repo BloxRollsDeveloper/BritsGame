1f89e9d1c525b71d1907ec65f97d98d92fc87fee
BloxRollsDeveloper <46302975+BloxRollsDeveloper@users.noreply.github.com>
2025-10-30 17:16:43 +0100

    Dialogue option
    t

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -1,18 +1,67 @@
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
+public class DialogueController : MonoBehaviour
 
-public class DialougeController : MonoBehaviour
+
+
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TextMeshProUGUI textComponent;
+    public float textSpeed;
+    public float characterPerSecond = 90;
+    public string[] lines;
+    private int _index;
+
     void Start()
     {
-        
+        textComponent.text = string.Empty;
+        StartDialogue();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            textComponent.text = lines[_index];
+        }
+    }
+
+    void StartDialogue()
+    {
+        _index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[_index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (_index < lines.Length - 1)
+        {
+            _index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
+
+
+
